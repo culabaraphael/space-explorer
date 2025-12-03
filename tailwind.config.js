ddd819b74b4b5471212d42a8cee08fcd3604b56a
Your Name <culabaraphael678@gmail.com>
2025-12-04 00:45:06 +0800

    Part 1 complete: Project setup with authentication working

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/View/Resources/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        forms,
+        require('daisyui'),
+    ],
+
+    daisyui: {
+        themes: ["dark"], // We'll use dark theme for space aesthetic
+    },
+};
